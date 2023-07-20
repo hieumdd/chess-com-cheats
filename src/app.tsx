a0d0ff90ee5f4d4c5b9993944f1ebf5406d5e8f6
HM <hieumdd@gmail.com>
2023-07-20 14:06:46 +0700

    feat: Update useChess

--- src/app.tsx
+++ src/app.tsx
@@ -1,84 +1,25 @@
-import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
-import { SettingsState, useSettingsStore } from './stores/settings.store';
 import { EvaluationOptions } from './services/evaluation.service';
+import { useChess } from './hooks/use-chess';
 
 function App() {
     const { register, watch } = useForm<EvaluationOptions>({
         defaultValues: {
-            color: 'white',
             lines: '1',
-            depth: '1',
+            depth: '31',
             enabled: false,
         },
     });
 
-    const enabledToggle = () => {
-        console.log(watch());
-    };
-
-    const onClick = () => {
-        chrome.tabs.query({});
-        chrome.tabs
-            .query({
-                active: true,
-                url: 'https://www.chess.com/play/computer',
-            })
-            .then(async ([tab]) => {
-                console.log(tab);
-                return chrome.tabs.sendMessage(tab.id as number, { type: 'GET_PGN' });
-            })
-            .then((response) => console.log(response))
-            .catch((error) => console.log(error));
-    };
-
-    // useEffect(() => {
-    //     const x = setInterval(
-    //         () =>
-    //             chrome.tabs
-    //                 .query({ active: true, currentWindow: true })
-    //                 .then(([tab]) => {
-    //                     return chrome.tabs.sendMessage(tab.id as number, { type: 'GET_PGN' });
-    //                 })
-    //                 .then((response) => console.log(response))
-    //                 .catch((error) => console.log(error)),
-    //         500,
-    //     );
-
-    //     return () => clearInterval(x);
-    // }, []);
+    const { command, commandValue } = useChess(watch());
 
     return (
         <div className="p-2 flex flex-col gap-4">
             <div className="card card-compact shadow">
-                <div className="card-body">
-                    <h1 className="card-title">Chess Helper</h1>
-                </div>
-            </div>
-            <div className="card card-compact shadow">
                 <form className="card-body">
                     <h2 className="card-title">Settings</h2>
                     <div className="grid grid-cols-2 gap-4">
-                        <h3>Color</h3>
-                        <div className="gap-2 form-control">
-                            {(
-                                [
-                                    ['white', 'White'],
-                                    ['black', 'Black'],
-                                ] as const
-                            ).map(([value, label]) => (
-                                <label key={value} className="p-0 label cursor-pointer">
-                                    <span className="label-text">{label}</span>
-                                    <input
-                                        {...register('color')}
-                                        type="radio"
-                                        value={value}
-                                        className="radio checked:bg-slate-500"
-                                    />
-                                </label>
-                            ))}
-                        </div>
                         <h3>Lines</h3>
                         <div className="form-control">
                             <input
@@ -101,11 +42,7 @@ function App() {
                         </div>
                         <h3>Enabled</h3>
                         <div className="form-control">
-                            <input
-                                {...register('enabled', { onChange: enabledToggle })}
-                                type="checkbox"
-                                className="toggle"
-                            />
+                            <input {...register('enabled')} type="checkbox" className="toggle" />
                         </div>
                     </div>
                 </form>
@@ -113,8 +50,12 @@ function App() {
             <div className="card card-compact shadow">
                 <div className="card-body">
                     <h2 className="card-title">Evaluation</h2>
-                    <p>{JSON.stringify(watch())}</p>
-                    <button onClick={onClick}> ABC</button>
+                    <div className="grid grid-cols-2 gap-4">
+                        <p>Command</p>
+                        <p>{command || 'N/A'}</p>
+                        <p>Command Value</p>
+                        <p>{commandValue || 'N/A'}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/hooks/use-chess.ts
+++ src/hooks/use-chess.ts
@@ -1,17 +1,52 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Chess } from 'chess.js';
 
 import { EvaluationOptions } from '../services/evaluation.service';
 
-export const useChess = ({ enabled }: EvaluationOptions) => {
+export const useChess = ({ lines, depth, enabled }: EvaluationOptions) => {
     const timerRef = useRef<number | null>(null);
+    const gameRef = useRef(new Chess());
+    const stockfish = useRef(new Worker('stockfish.js'));
+
+    const [fen, setFen] = useState('start');
+    const [command, setCommand] = useState('');
+    const [commandValue, setCommandValue] = useState('');
 
     useEffect(() => {
         if (enabled) {
             timerRef.current = setInterval(() => {
-                console.log(123);
-            }, 5000);
+                chrome.tabs.query({ active: true, url: '*://*.chess.com/play/*' }).then(([tab]) => {
+                    if (tab) {
+                        chrome.tabs.sendMessage(tab.id as number, { type: 'GET_PGN' }).then((pgn) => {
+                            gameRef.current.loadPgn(pgn);
+                            setFen(gameRef.current.fen());
+                        })
+                    }
+                })
+            }, 2_000)
         } else {
             clearInterval(timerRef.current as number);
         }
-    });
+
+        return () => clearInterval(timerRef.current as number);
+    }, [enabled]);
+
+    useEffect(() => {
+        stockfish.current.onmessage = (e) => {
+            const [command, ...args] = e.data.split(' ');
+            setCommand(command);
+            setCommandValue(args.join(' '))
+        };
+    }, [])
+
+    useEffect(() => {
+        stockfish.current.postMessage(`setoption name MultiPV value ${lines}`);
+    }, [lines])
+
+    useEffect(() => {
+        stockfish.current.postMessage(`position fen ${fen}`);
+        stockfish.current.postMessage(`go ${depth}`);
+    }, [fen, depth])
+
+    return { command, commandValue }
 };
