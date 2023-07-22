60f1eb8f47b3dac38cddbe6ed83ab7cdd235473c
HM <hieumdd@gmail.com>
2023-07-22 21:21:27 +0700

    feat: Update content.ts for matchmaking scrape

--- src/content.ts
+++ src/content.ts
@@ -1,13 +1,15 @@
 import { Message } from './message.type';
 
 chrome.runtime.onMessage.addListener((message: Message, _, sendResponse) => {
-    const pgn = [...document.querySelectorAll('vertical-move-list > .move')].map((turnEl, i) => {
-        const [whiteMove, blackMove] = [...turnEl.children].map((moveEl) => {
+    const pgn = [...document.querySelectorAll('vertical-move-list > .move')].map((turnEl) => {
+        const turn = turnEl.getAttribute('data-whole-move-number') ?? '';
+
+        const [whiteMove, blackMove] = [...turnEl.querySelectorAll('.node')].map((moveEl) => {
             const piece = moveEl.children[0]?.getAttribute('data-figurine') ?? '';
             const move = moveEl.textContent ?? '';
             return move?.endsWith('=') ? move + piece : piece + move;
         });
-        return `${i + 1}. ${whiteMove ?? ''} ${blackMove ?? ''}`;
+        return `${turn}. ${whiteMove ?? ''} ${blackMove ?? ''}`;
     });
 
     sendResponse(pgn.join(' \n'));
